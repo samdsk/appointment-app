7cfca4a2c232b73c94a02469e353d19882eb1fdd
samdsk <sam.ds@live.com>
2024-03-13 12:48:54 +0100

    init

---
+++ src/main/java/com/example/appointment_app/AppointmentAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.appointment_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppointmentAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppointmentAppApplication.class, args);
+	}
+
+}
